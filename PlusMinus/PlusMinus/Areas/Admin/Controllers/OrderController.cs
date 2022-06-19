9832a7b723d685139bd3e037cf8a8cbf0a3cc04a
DiabloCode <maslovgeorge1@gmail.com>
2022-06-19 20:05:26 +0300

    Added order controller

--- PlusMinus/PlusMinus/Areas/Admin/Controllers/OrderController.cs
+++ PlusMinus/PlusMinus/Areas/Admin/Controllers/OrderController.cs
@@ -3,15 +3,54 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PlusMinus.BLL.Interfaces;
+using PlusMinus.Core.Models;
 
 namespace PlusMinus.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Order> orders = _orderService.GetOrders(o => o.OrderId > 0); 
+            
+            return View(orders);
+        }
+
+        [HttpGet]
+        public IActionResult ChangeOrderStatus(int id)
+        {
+            Order order = _orderService.GetOrderByID(id);
+
+            if (order is null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        [HttpPost]
+        public IActionResult ChangeOrderStatus(Order orderViewModel)
+        {
+            if (orderViewModel.OrderId < 0)
+            {
+                return NotFound();
+            }
+
+            Order order = _orderService.GetOrderByID(orderViewModel.OrderId);
+            order.Status = orderViewModel.Status;
+            _orderService.UpdateOrder(order);
+
+            return RedirectToAction("Index");
         }
     }
 }
