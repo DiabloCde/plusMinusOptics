61c5dcfe21a643fd6e66263e685eac81e2b5d8a4
VlaD1377 <vladlabunets2003@gmail.com>
2022-06-20 20:14:09 +0300

    Added UI for Shop and Profile pages. Started controller for shop

--- PlusMinus/PlusMinus/Areas/Customer/Controllers/CartController.cs
+++ PlusMinus/PlusMinus/Areas/Customer/Controllers/CartController.cs
@@ -3,15 +3,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PlusMinus.BLL.Interfaces;
+using PlusMinus.Core.Models;
 
 namespace PlusMinus.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class CartController : Controller
     {
+        
+        IProductService<Product> _productService;
+        
+        public CartController(IProductService<Product> productService)
+        {
+            _productService = productService;
+        }
+        
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Shop()
+        {
+            System.Linq.Expressions.Expression<Func<Product, bool>> expr = i => i.ProductId >= 0;
+            var prods = _productService.GetProducts(expr);
+            return View(prods);
         }
     }
 }
--- PlusMinus/PlusMinus/Areas/Customer/Controllers/HomeController.cs
+++ PlusMinus/PlusMinus/Areas/Customer/Controllers/HomeController.cs
@@ -3,13 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PlusMinus.BLL.Interfaces;
+using PlusMinus.Core.Models;
 
 namespace PlusMinus.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
+        IUserService _userService;
+        
+        public HomeController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
         {
             return View();
         }
